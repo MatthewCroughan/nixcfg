f08ffab28aec5371f634dc1501e0964536b105dc
matthewcroughan <matt@croughan.sh>
2020-10-26 06:25:32 +0000

    fix indentation

--- users/matthew/default.nix
+++ users/matthew/default.nix
@@ -11,13 +11,13 @@
 
   programs.home-manager.enable = true;
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "20.03";
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
 }
