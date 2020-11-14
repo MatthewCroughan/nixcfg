210f345296f696dfbff0dc52ea0172cebc9fe178
matthewcroughan <matt@croughan.sh>
2020-11-14 06:23:06 +0000

    the fix is in

--- flake.nix
+++ flake.nix
@@ -3,25 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
     nixpkgs-wayland.url = "github:colemickens/nixpkgs-wayland";
     home-manager.url = "github:nix-community/home-manager";
     firefox.url = "github:colemickens/flake-firefox-nightly";
     firefox.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, home-manager, nixpkgs, ... }: {
+  outputs = inputs:
+    /* ignore:: */ let ignoreme = ({config,lib,self,home-manager,nixpkgs,...}: with lib; { system.nixos.revision = mkForce null; system.nixos.versionSuffix = mkForce "pre-git"; }); in
+  {
     nixosConfigurations = {
-      t480 = nixpkgs.lib.nixosSystem {
+      t480 = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           (import ./hosts/t480/configuration.nix)
-          home-manager.nixosModules.home-manager
+          inputs.home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users = import ./users self; # pass 'self' in order to allow ./users/default.nix -> ./users/matthew/default.nix to access ${self}, to provide a path relative to flake.nix.
+            home-manager.users = import ./users inputs.self; # pass 'self' in order to allow ./users/default.nix -> ./users/matthew/default.nix to access ${self}, to provide a path relative to flake.nix.
           }
         ];
+        specialArgs = { inherit inputs; };
       };
     };
   };
