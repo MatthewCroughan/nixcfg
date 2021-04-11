c468597932b6f85b2c97ca6213a03fa35ecd15d2
matthewcroughan <matt@croughan.sh>
2021-04-11 22:40:53 +0100

    pkgs::parsecgaming: update
    based on https://github.com/delroth/infra.delroth.net/blob/5fd4c9e5be9c17569149ce742d1902d4f87cfd52/pkgs/parsec.nix

--- pkgs/parsecgaming/default.nix
+++ pkgs/parsecgaming/default.nix
@@ -1,65 +1,78 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
+{ lib, stdenv, fetchurl, alsaLib, dbus, libGL, libpulseaudio, libva
+, openssl, udev, xorg, wayland }:
 
-let debExtract = stdenv.mkDerivation {
-  name = "parsecgaming-pkg";
+stdenv.mkDerivation {
+  pname = "parsec";
+  version = "2021-01-12";
+
   src = fetchurl {
-    url = "https://s3.amazonaws.com/parsec-build/package/parsec-linux.deb";
+    url = "https://builds.parsecgaming.com/package/parsec-linux.deb";
     sha256 = "1hfdzjd8qiksv336m4s4ban004vhv00cv2j461gc6zrp37s0fwhc";
-    };
-  phases = [ "buildPhase" ];
-  buildInputs = [ dpkg ];
-  buildPhase = ''
-    mkdir $out
-    dpkg-deb -x $src $out
-    # dpkg-deb makes $out group-writable, which nix doesn't like
-    chmod 755 $out
-    mv $out/usr/* $out
-    rmdir $out/usr
-  '';
-};
-in buildFHSUserEnv {
-  name = "parsecgaming";
-  targetPkgs = pkgs: with pkgs; [
-    alsaLib
-    wayland
-    cups
-    dbus
-    debExtract
-    fontconfig
-    freetype
-    libGL
-    libpulseaudio
-    libsamplerate
-    libudev
-    libva
-    libxkbcommon
-    nas
-    stdenv.cc.cc.lib
-    vulkan-loader
-    xorg.libX11
-    xorg.libXScrnSaver
-    xorg.libXcursor
-    xorg.libXext
-    xorg.libXi
-    xorg.libXinerama
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXxf86vm
-    xorg.libxcb
-    # Those libraries are missing from buildInputs
-    # libGLES_CM.so.1        # Can't find in my /nix/store
-    # libudev.so.0           # Covered by libudev.so.1
-    # libva.so.1             # Covered by libva.so.2
-    # libwayland-client.so.0 # No wayland support
-    # libwayland-cursor.so.0 # No wayland support
-    # libwayland-egl.so.1    # No wayland support
-  ];
-  runScript = "/usr/bin/parsecd";
+  };
 
-  meta = {
-    description = "Parsec Gaming Daemon";
-    homepage = https://www.parsecgaming.com/;
+  # The upstream deb package is out of date and doesn't work out of the box
+  # anymore due to api.parsecgaming.com being down. Auto-updating doesn't work
+  # because it doesn't patchelf the dynamic dependencies. Instead, "manually"
+  # fetch the latest binaries.
+  latest_appdata = fetchurl {
+    url = "https://builds.parsecgaming.com/channel/release/appdata/linux/latest";
+    sha256 = "01wgg9nvny4ya0rzjrhi5h61wkwhn0g2dirdbylj1a554rz88n31";
+  };
+  latest_parsecd_so = fetchurl {
+    url = "https://builds.parsecgaming.com/channel/release/binary/linux/gz/parsecd-150-68.so";
+    sha256 = "13325rj9fm2xzvn2i2svncicsc807a9nh5lvwk10bcvxv4s6mrf5";
+  };
+
+  postPatch = ''
+    cp $latest_appdata usr/share/parsec/skel/appdata.json
+    cp $latest_parsecd_so usr/share/parsec/skel/parsecd-150-68.so
+  '';
+
+  runtimeDependencies = [
+    alsaLib (lib.getLib dbus) libGL libpulseaudio libva.out
+    (lib.getLib openssl) (lib.getLib stdenv.cc.cc) (lib.getLib udev)
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXinerama xorg.libXrandr
+    xorg.libXScrnSaver wayland
+  ];
+
+  unpackPhase = ''
+    ar p "$src" data.tar.xz | tar xJ
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin $out/libexec
+    cp usr/bin/parsecd $out/libexec
+    cp -r usr/share/parsec/skel $out/libexec
+    # parsecd is a small wrapper binary which copies skel/* to ~/.parsec and
+    # then runs from there. Unfortunately, it hardcodes the /usr/share/parsec
+    # path, and patching that would be annoying. Instead, just reproduce the
+    # install logic in a wrapper script.
+    cat >$out/bin/parsecd <<EOF
+    #! /bin/sh
+    mkdir -p \$HOME/.parsec
+    ln -sf $out/libexec/skel/* \$HOME/.parsec
+    exec $out/libexec/parsecd
+    EOF
+    chmod +x $out/bin/parsecd
+  '';
+
+  postFixup = ''
+    # We do not use autoPatchelfHook since we need runtimeDependencies rpath to
+    # also be set on the .so, not just on the main executable.
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        $out/libexec/parsecd
+    rpath=""
+    for dep in $runtimeDependencies; do
+      rpath="$rpath''${rpath:+:}$dep/lib"
+    done
+    patchelf --set-rpath "$rpath" $out/libexec/parsecd
+    patchelf --set-rpath "$rpath" $out/libexec/skel/*.so
+  '';
+
+  meta = with lib; {
+    description = "Remotely connect to a gaming PC for a low latency remote computing experience";
+    homepage = "https://parsecgaming.com/";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ delroth ];
   };
 }
-
