27f6a85c46cfc2c1af3bfc01247eaf0c112a996a
matthewcroughan <matt@croughan.sh>
2021-04-14 01:14:43 +0100

    profiles::wireless: add Astral_Ship_5GHz

--- profiles/wireless.nix
+++ profiles/wireless.nix
@@ -1,6 +1,7 @@
 {
   # Configures a list of networks I want to connect to.
   networking.wireless.networks = { 
+    Astral_Ship_5GHz.pskRaw = "157bddfe695338f0fc6a2cfa88dbb6134a3489579f7e748714fa1ae2e8a82192";
     Astral_Ship.pskRaw = "ff866b7b9494bd6915c28a06c8604d1e2396e590e64f71b2fdf9c0c9709ec2c4";
     Astral_ship_Festri.pskRaw = "a20c6252f348eddc73efc0a0956c7cf66c958a327d7d7b943d72cf477008d8f2";
     DoESLiverpool.pskRaw = "63e49f779a41eda7be1510a275a07e519d407af706d0f2d3cc3140b9aecd412f"; 
