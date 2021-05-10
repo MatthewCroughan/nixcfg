3ac4bd4550e4e4047bb185d72ddaeeb29017fa77
matthewcroughan <matt@croughan.sh>
2021-05-10 03:34:07 +0100

    mixins::obs: add obs-ndi based on flake-ndi in inputs
    This uses my 'flake-ndi' flake in order to override licensing behavior via requireFile in pkgs.ndi which otherwise requires manual interaction, additionally it symlinks store paths into .config/obs-studio in order to make the pluging load.

--- mixins/obs.nix
+++ mixins/obs.nix
@@ -6,11 +6,16 @@
     programs.sway.extraPackages = []; # block rxvt
 
     home-manager.users.matthew = { pkgs, ... }: {
+
+      home.file.".config/obs-studio/plugins/obs-ndi/bin/64bit/obs-ndi.so".source = "${pkgs.obs-ndi}/lib/obs-plugins/obs-ndi.so";
+      home.file.".config/obs-studio/plugins/obs-ndi/data/locale/en-US.ini".source = "${pkgs.obs-ndi}/share/obs/obs-plugins/obs-ndi/locale/en-US.ini";
+
       programs.obs-studio = {
         enable = true;
         plugins = with pkgs; [
           obs-wlrobs
           obs-v4l2sink
+          obs-ndi
         ];
       };
     };
