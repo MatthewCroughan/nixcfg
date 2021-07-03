55b22d6185f8d0e5a17955463f462b94d6e714aa
matthewcroughan <matt@croughan.sh>
2021-07-03 09:20:53 +0100

    h-m::obs-studio: disable plugins
    Most of the plugins have been implemented by default now upstream, and I don't really need NDI at the moment.

--- mixins/obs.nix
+++ mixins/obs.nix
@@ -7,16 +7,16 @@
 
     home-manager.users.matthew = { pkgs, ... }: {
 
-      home.file.".config/obs-studio/plugins/obs-ndi/bin/64bit/obs-ndi.so".source = "${pkgs.obs-ndi}/lib/obs-plugins/obs-ndi.so";
-      home.file.".config/obs-studio/plugins/obs-ndi/data/locale/en-US.ini".source = "${pkgs.obs-ndi}/share/obs/obs-plugins/obs-ndi/locale/en-US.ini";
+#      home.file.".config/obs-studio/plugins/obs-ndi/bin/64bit/obs-ndi.so".source = "${pkgs.obs-ndi}/lib/obs-plugins/obs-ndi.so";
+#      home.file.".config/obs-studio/plugins/obs-ndi/data/locale/en-US.ini".source = "${pkgs.obs-ndi}/share/obs/obs-plugins/obs-ndi/locale/en-US.ini";
 
       programs.obs-studio = {
         enable = true;
-        plugins = with pkgs; [
-          obs-wlrobs
-          obs-v4l2sink
-          obs-ndi
-        ];
+#        plugins = with pkgs; [
+#          obs-wlrobs
+#          obs-v4l2sink
+#          obs-ndi
+#        ];
       };
     };
   };
