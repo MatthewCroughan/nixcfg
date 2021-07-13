e3b324fa62fe5b6cb15813e1d1ca7a57e6a5440b
matthewcroughan <matt@croughan.sh>
2021-07-13 02:48:02 +0100

    hosts::t480: fix wireless configuration
    The interface name seems to have changed to wlan0, and the latest NixOS wants networking.wireless.interfaces to be set, else a warning is shown, so I have filled it.

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -68,14 +68,17 @@
   services.avahi.publish.enable = true;
 
   # Enables wireless support via wpa_supplicant.
-  networking.wireless.enable = true;
+  networking.wireless = {
+    enable = true;
+    interfaces = [ "wlan0" ];
+  };
   
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
+  networking.interfaces.wlan0.useDHCP = true;
 
   # Setup ZFS requirements
   boot.supportedFilesystems = [ "zfs" ];
