e30339dbb98caac7d16c405077b9ca32d5120715
matthewcroughan <matt@croughan.sh>
2021-09-29 05:01:53 +0100

    t480: cleanup formatting

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -4,21 +4,16 @@
 
 { config, pkgs, inputs, ... }:
 
-#let
-#  firefoxFlake = inputs.firefox.packages.${pkgs.system};
-#in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
-#      ./modules/wireguard.nix
-#      ../../profiles/dark-theme.nix
-#      ../../modules/ly.nix
       ../../profiles/tailscale.nix
       ../../profiles/sway.nix
       ../../profiles/steam.nix
       ../../profiles/wireless.nix
       ../../profiles/pipewire.nix
+      ../../profiles/avahi.nix
       ../../mixins/obs.nix
       ../../mixins/v4l2loopback.nix
       ../../mixins/editor/vim.nix
@@ -43,18 +38,30 @@
     linkInputs = true;
   };
 
-  services.resolved = {
-    enable = true;
-    dnssec = "false";
-  };
-
-  networking.useNetworkd = true;
-
   # This happens to fix a problem with the systemd service that is created as a
   # result of enabling networkd. It's not clear why it happens, but I should
   # re-evaluate whether this is necessary to set in the future.
   systemd.services.systemd-networkd-wait-online.enable = false;
 
+  networking = {
+    hostName = "t480";
+    useNetworkd = true;
+    wireless = {
+      enable = true;
+      interfaces = [ "wlp3s0" ];
+    };
+    useDHCP = false;
+    interfaces = {
+      "enp0s31f6".useDHCP = true;
+      "wlp3s0".useDHCP = true;
+    };
+  };
+
+  services.resolved = {
+    enable = true;
+    dnssec = "false";
+  };
+    
   services.throttled.enable = true;
 
   services.logind.killUserProcesses = true;
@@ -77,34 +84,16 @@
   # Gives access to the NUR: https://github.com/nix-community/NUR
   nixpkgs.overlays = [ inputs.nur.overlay inputs.flake-ndi.overlay ];
 
-  # Use the systemd-boot EFI boot loader, instead of GRUB, etc.
-
-  boot.loader.systemd-boot = {
-    enable = true;
-    configurationLimit = 10;
+  # Use the systemd-boot EFI boot loader, instead of GRUB.
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    efi = {
+      canTouchEfiVariables = true;
+    };
   };
-
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Set hostname.
-  networking.hostName = "t480";
-
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-  services.avahi.publish.enable = true;
-
-  # Enables wireless support via wpa_supplicant.
-  networking.wireless = {
-    enable = true;
-    interfaces = [ "wlp3s0" ];
-  };
-  
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
 
   # Setup ZFS requirements
   boot.supportedFilesystems = [ "zfs" ];
@@ -119,10 +108,6 @@
 
   # Allow trim on SSD
   boot.initrd.luks.devices.crypted.allowDiscards = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -140,62 +125,22 @@
   # Enable OpenGL
   hardware.opengl.enable = true;
 
-#  services.xserver.displayManager.ly.enable = true;
-#  services.xserver.displayManager.lightdm.enable = true;
-#  services.xserver.displayManager.sddm.enable = true;
-
-#  services.xserver.displayManager.gdm.wayland = true;
-#  services.xserver.displayManager.gdm.enable = true;
-#  services.xserver.enable = true;
-
-##services.xserver.desktopManager.gnome3.enable = true;
-#services.xserver.desktopManager.xfce.enable = true;
-#programs.xwayland.enable = false;
-
-#  # Enable the X11 windowing system.
-#  services.xserver.enable = false;
-#  services.xserver.layout = "us";
-#
-#  # Enable touchpad support.
-#  services.xserver.libinput.enable = true;
-#
-#  # Enable i3  
-#  services.xserver.windowManager.i3.enable = true;
-
-#  services.xserver = {
-#    enable = false;
-#
-#    desktopManager = {
-#      xterm.enable = false;
-#    };
-#   
-#    displayManager = {
-#        defaultSession = "none+sway";
-#        lightdm.enable = true;
-#    };
-#
-#    windowManager.i3 = {
-#      enable = false;
-#      extraPackages = with pkgs; [
-#        dmenu #application launcher most people use
-#        i3status # gives you the default i3 status bar
-#        i3lock #default i3 screen locker
-#        i3blocks #if you are planning on using i3blocks over i3status
-#     ];
-#    };
-#  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     wget chromium vim tmux mpv gnumake htop inputs.agenix.defaultPackage.x86_64-linux
-   ];
+  environment.systemPackages = with pkgs; [
+    wget
+    chromium
+    vim
+    tmux
+    mpv
+    gnumake
+    htop
+    inputs.agenix.defaultPackage.x86_64-linux
+  ];
 
   # Allow proprietary software.
   nixpkgs.config.allowUnfree = true;
 
   # Enable Docker.
-  virtualisation.docker.enable= true;
+  virtualisation.docker.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -230,54 +175,10 @@
    };
  };
 
-
- # Udev Rules
-   
+ # Udev Rule for my Gamecube Controller Adapter
  services.udev.extraRules = ''SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"'';
 
  networking.firewall.enable = false;
 
 }
 
-## UNUSED ##
-
-  #nixpkgs.config.packageOverrides = pkgs: {
-  #  chromium = pkgs.chromium.override {
-  #    useOzone = true;
-  #  };
-  #};
-
-  ## Some programs need SUID wrappers, can be configured further or are
-  ## started in user sessions.
-  #programs.mtr.enable = true;
-  #programs.gnupg.agent = {
-  #  enable = true;
-  #  enableSSHSupport = true;
-  #  pinentryFlavor = "gnome3";
-  #};
-
-  ## Open ports in the firewall.
-  #networking.firewall.allowedTCPPorts = [ ... ];
-  #networking.firewall.allowedUDPPorts = [ ... ];
-
-  ## Or disable the firewall altogether.
-  #networking.firewall.enable = false;
-
-  #networking.networkmanager.unmanaged = [
-  #   "*" "except:type:wwan" "except:type:gsm"
-  #];
-
-  ## Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  ## Enable the X11 windowing system.
-  #services.xserver.enable = true;
-  #services.xserver.layout = "us";
-  #services.xserver.xkbOptions = "eurosign:e";
-
-  ## Enable touchpad support.
-  #services.xserver.libinput.enable = true;
-
-  ## Enable the KDE Desktop Environment.
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
