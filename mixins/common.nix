ce86bee2755127a4fdaca91e5e037d3fe625cba9
matthewcroughan <matt@croughan.sh>
2021-09-29 04:35:13 +0100

    common: derive channels and registry from inputs
    Now my nix channels and registry entries match my inputs entirely,
    meaning if I run 'nix-shell -p hello' or 'nix shell nixpkgs#hello', the
    'hello' program will be at the version from the nixpkgs of the input in
    my flake. This keeps me sane.'

--- mixins/common.nix
+++ mixins/common.nix
@@ -1,3 +1,14 @@
+{ config, pkgs, lib, inputs, ...}:
 {
-  nix.trustedUsers = [ "@wheel" "root" ];
+  nix = {
+    extraOptions =
+      let empty_registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}''; in
+      ''
+        experimental-features = nix-command flakes recursive-nix
+        flake-registry = ${empty_registry}
+        builders-use-substitutes = true
+      '';
+    trustedUsers = [ "@wheel" "root" ];
+    registry.nixpkgs.flake = inputs.nixpkgs;
+  };
 }
