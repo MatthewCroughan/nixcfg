6d254ae328815d3c5f821566298eb7e41ccb30fa
matthewcroughan <matt@croughan.sh>
2021-10-13 03:50:45 +0100

    wireless: add sirGranite wifi

--- profiles/wireless.nix
+++ profiles/wireless.nix
@@ -6,6 +6,7 @@
     Astral_Ship.pskRaw = "ff866b7b9494bd6915c28a06c8604d1e2396e590e64f71b2fdf9c0c9709ec2c4";
     Astral_ship_Festri.pskRaw = "a20c6252f348eddc73efc0a0956c7cf66c958a327d7d7b943d72cf477008d8f2";
     DoESLiverpool.pskRaw = "63e49f779a41eda7be1510a275a07e519d407af706d0f2d3cc3140b9aecd412f"; 
+    sirGranite.pskRaw = "58f5e6d6058c8b476e7a88f0107d832da12e2ed1b1ecacc071f61345c4731eec"; 
     bebop = { 
       pskRaw = "0c89c6e287005f99efda5199d432f94cf8d08ea7925ba2d24eef24e268aabe67"; 
       priority = 10; 
