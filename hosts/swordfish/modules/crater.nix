bec76dfedb2854c51f8de4ccd3f9661c2b67dc61
matthewcroughan <matt@croughan.sh>
2021-10-20 00:29:40 +0100

    swordfish: update crater module

--- hosts/swordfish/modules/crater.nix
+++ hosts/swordfish/modules/crater.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, ... }:
 let
-  matthewcroughan = builtins.getFlake "github:matthewcroughan/nixpkgs/97b7da1dd6f7ff649a4071bf9ac28622544cf91e";
+  matthewcroughan = builtins.getFlake "github:matthewcroughan/nixpkgs/4fc9b7816c95125e2bc80be527847a55acdcb432";
 in
 {
   imports = [
     (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/matthewcroughan/nixpkgs/97b7da1dd6f7ff649a4071bf9ac28622544cf91e/nixos/modules/services/web-apps/crater.nix";
-      sha256 = "18sxvwwwpz9x2rjaj5b031ddp6vqxxi9mbig4wk8bdrxhb9mi7df";
+      url = "https://raw.githubusercontent.com/matthewcroughan/nixpkgs/4fc9b7816c95125e2bc80be527847a55acdcb432/nixos/modules/services/web-apps/crater.nix";
+      sha256 = "0m7lxgf08lh54c12jzwxifzh8dm4i117c8ifz51vgawjmhfi5is3";
     })
   ];
 
