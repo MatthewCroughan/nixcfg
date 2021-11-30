2ff3a815351c14b6eaa2a25997cd6e8c390775b9
matthewcroughan <matt@croughan.sh>
2021-11-30 07:55:16 +0000

    swordfish: use my own hydroxide package
    The nixpkgs one is fscked so I'm gonna use my own.

--- hosts/swordfish/modules/hydroxide.nix
+++ hosts/swordfish/modules/hydroxide.nix
@@ -1,9 +1,24 @@
-{ config, lib, inputs, ... }:
+{ config, lib, pkgs, inputs, ... }:
+let
+  hydroxideOverride = pkgs.buildGoModule rec {
+    pname = "hydroxide";
+    version = "0.2.21";
+    vendorSha256 = "sha256-M5QlhF2Cj1jn5NNiKj1Roh9+sNCWxQEb4vbtsDfapWY=";
+    GO111MODULE="on";
+    src = pkgs.fetchFromGitHub {
+      owner = "emersion";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-fF+pQnqAWBktc4NdQFTHeB/sEg5bPTxXtdL1x5JuXU8=";
+    };
+  };
+in
 {
   imports = [ "${inputs.self}/modules/hydroxide.nix" ];
 
   services.hydroxide = {
     enable = true;
     host = "0.0.0.0";
+    package = hydroxideOverride;
   };
 }
