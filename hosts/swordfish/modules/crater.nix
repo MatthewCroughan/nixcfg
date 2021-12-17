2c18ad53dfce8240d9eddb98fb9bb4ca11637651
matthewcroughan <matt@croughan.sh>
2021-12-17 02:28:52 +0000

    crater: add terrible workarounds
    This is one incredibly broken piece of software.

--- hosts/swordfish/modules/crater.nix
+++ hosts/swordfish/modules/crater.nix
@@ -11,9 +11,24 @@ let
     autoStart = true;
     ephemeral = false;
     config = { config, pkgs, ... }: {
+      systemd.tmpfiles.rules = [
+        "L+ ${config.services.crater.dataDir}/public/storage 0755 ${config.services.crater.user} ${config.services.crater.group} - ${pkgs.crater}/storage/app/public"
+      ];
       imports = [
         craterModule
       ];
+      systemd.services.phpfpm-crater.postStart = ''
+        until [[ -s ${config.services.crater.dataDir}/database.sqlite && $(${pkgs.sqlite}/bin/sqlite3 ${config.services.crater.dataDir}/database.sqlite "select credentials from file_disks;") ]]
+        do
+          sleep 1
+          echo "DATABASE IS FUCKING EMPTY"
+        done
+        STORAGE_PUBLIC=`${pkgs.sqlite}/bin/sqlite3 ${config.services.crater.dataDir}/database.sqlite "select credentials from file_disks where id=1;" | ${pkgs.jq}/bin/jq -r | ${pkgs.jq}/bin/jq '.root = "${config.services.crater.dataDir}/storage/app/public"' | ${pkgs.jq}/bin/jq -c`
+        STORAGE=`${pkgs.sqlite}/bin/sqlite3 ${config.services.crater.dataDir}/database.sqlite "select credentials from file_disks where id=2;" | ${pkgs.jq}/bin/jq -r | ${pkgs.jq}/bin/jq '.root = "${config.services.crater.dataDir}/storage/app"' | ${pkgs.jq}/bin/jq -c`
+
+        ${pkgs.sqlite}/bin/sqlite3 ${config.services.crater.dataDir}/database.sqlite "UPDATE file_disks SET credentials = '$STORAGE_PUBLIC' where id=1;"
+        ${pkgs.sqlite}/bin/sqlite3 ${config.services.crater.dataDir}/database.sqlite "UPDATE file_disks SET credentials = '$STORAGE' where id=2;"
+      '';
       nixpkgs.overlays = [
         craterOverlay
       ];
