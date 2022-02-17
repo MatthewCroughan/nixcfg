997516104bea3f6a7a514f35258d72a4fcd5d1f5
matthewcroughan <matt@croughan.sh>
2022-02-17 03:19:46 +0000

    secrets: refactor and rekey
    Allows all users to edit secrets. Systems remain the same.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -8,20 +8,22 @@ let
     matthew-t480 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClm+SMN9Bg1HZ+MjH1VQYEXAnslGWT9564pj/KGO79WMQLUxdp3WWa1hQadf2PleAIEFEul3knrpRSEK3yHcCk3g+sCh3XIJcFZLesswe0V+kCAw+JBSd18ESJ4Qko+iDK95cDzucLFwXB10FMVKQCrX90KR+Fp6s6eJHcZGmpxTPgNulDpAjM2APluM3xBCe6zZzt+iNIzn3J8PRKbpNNbuw/LMRU8+udrGbLavUMcSk7ER9pAyLGhz//9aHWDPu7ZRje+vTWgnGFpzbtEzdjnP+2v45nLKWG7o7WdTAsAR8WSccjtNoBiVgSmpHr07zJ0/gTeL4PUkk3lbtzF/PdtTQGm3Ng4SjOBlhRVaTuKBlF2X/Rwq+W4LCbHVgA79MyhJxL2TDbKBPUSLfckqxP89e8Q7iQ4XjIHqVb50ojNNLGcOQRrHq14Twwx/ZDDQvMXCsLwM6vyoYa8KdSaASEr1clx78qNp9PHGlr+UztW+EsoZI7j1tzcHMmq2BSK90= matthew@t480";
     matthew-swordfish = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD7jt+f8gURZcWeo9Sko5kdPW1ORd+CDXsCxyHgGRK5IVKLrluAEC7ha0xlOQ3WtHEiWj3P9/C9UzATjWaMZU5j/A7Ysib2peBFQZ0roYwHDYVZzutF7HJWBc4WW/CPSjWZfza2jC3e/Qe2ktmnbri0oq27EXYJwhs+/70+VA4NZ5d7xD1CnYhv4LdsudSTLJjsyCL6PLVbtUZKW8avyH359R4rLTkklk9H1d0RYGDkHKMFB5ADdlEJwnjRAbjK94u+M6jhN9ahMrfSsbLUd1DcASMoW7fw+Y16l//7cDr8rN32Mi1Vs/mBvJmnN//84QtqJM2UD/lxI/k8aJEXskojfmBP2qd+mevHfuA30b6BtngeDvh5RPnYYDL28g9iyDKMWtBEYMkyF+kbERiiOkxFpudyB7Vd0fYAZxusqq8EOpVnjj97T0hFbGspam/Q9H5tT4U02JG127Z2+ohq5DUYeiMDh3mOs5uc/R+svj00NJtmEuNdqjtyktYP6zfhiVs=";
   };
-in  
+  allUsers = builtins.attrValues users;
+  allSystems = builtins.attrValues systems;
+in
 {
-  "cloudflare_api_key.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "orbis-tertiusHerculesSecrets.age".publicKeys = [ users.matthew-t480 users.matthew-swordfish systems.swordfish ];
-  "orbis-tertiusHerculesClusterJoinToken.age".publicKeys = [ users.matthew-t480 users.matthew-swordfish systems.swordfish ];
-  "orbis-tertiusHerculesBinaryCaches.age".publicKeys = [ users.matthew-t480 users.matthew-swordfish systems.swordfish ];
-  "ardanaHerculesSecrets.age".publicKeys = [ users.matthew-t480 users.matthew-swordfish systems.swordfish ];
-  "ardanaHerculesClusterJoinToken.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "ardanaHerculesBinaryCaches.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "plutonomiconHerculesSecrets.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "plutonomiconHerculesClusterJoinToken.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "plutonomiconHerculesBinaryCaches.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "tunnelvrHerculesClusterJoinToken.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "tunnelvrHerculesBinaryCaches.age".publicKeys = [ users.matthew-swordfish systems.swordfish ];
-  "coturn_static_auth.age".publicKeys = [ users.matthew-t480 systems.matrix ];
-  "synapse_secrets_yaml.age".publicKeys = [ users.matthew-t480 systems.matrix ];
+  "cloudflare_api_key.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "orbis-tertiusHerculesSecrets.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "orbis-tertiusHerculesClusterJoinToken.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "orbis-tertiusHerculesBinaryCaches.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "ardanaHerculesSecrets.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "ardanaHerculesClusterJoinToken.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "ardanaHerculesBinaryCaches.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "plutonomiconHerculesSecrets.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "plutonomiconHerculesClusterJoinToken.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "plutonomiconHerculesBinaryCaches.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "tunnelvrHerculesClusterJoinToken.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "tunnelvrHerculesBinaryCaches.age".publicKeys = allUsers ++ [ systems.swordfish ];
+  "coturn_static_auth.age".publicKeys = allUsers ++ [ systems.matrix ];
+  "synapse_secrets_yaml.age".publicKeys = allUsers ++ [ systems.matrix ];
 }
