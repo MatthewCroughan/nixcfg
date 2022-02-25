fc0cbe2992c43db038fd832b902dc4bb7ab11237
matthewcroughan <matt@croughan.sh>
2022-02-25 21:26:49 +0000

    pyxis: update

--- hosts/pyxis/default.nix
+++ hosts/pyxis/default.nix
@@ -6,25 +6,25 @@
   androidVersion = 11;
 
   source.dirs."device/xiaomi/pyxis".src = builtins.fetchGit {
-    rev = "35b094bca0753e717502d543fc4fed444c84f5c1";
+    rev = "59e33094ff575311ecda0fc983451e8bac93582a";
     ref = "lineage-18.1";
     url = "https://github.com/SDM710-Development/android_device_xiaomi_pyxis.git";
   };
 
   source.dirs."device/xiaomi/sdm710-common".src = builtins.fetchGit {
-    rev = "2fd487af25b1d688c7528f26cf6f9e92afdddbef";
+    rev = "b831a000fed57d5f66870490257098157ea7c076";
     ref = "lineage-18.1";
     url = "https://github.com/SDM710-Development/android_device_xiaomi_sdm710-common.git";
   };
 
   source.dirs."kernel/xiaomi/sdm710".src = builtins.fetchGit {
-    rev = "c4d48d66ae60ac6b027bdbb2b618cef6ac245179";
+    rev = "beaeaddf970636fe486068484574c13f44248c11";
     ref = "10.0-caf";
     url = "https://github.com/SDM710-Development/android_kernel_xiaomi_sdm710.git";
   };
 
   source.dirs."vendor/xiaomi".src = builtins.fetchGit {
-    rev = "cea60d367ffc7375f410f6aac22c531d1b0c3e37";
+    rev = "ba97e497ec4b8cc4f2bb14185a3f2f8d6ad454a8";
     ref = "lineage-18.1";
     url = "https://github.com/SDM710-Development/proprietary_vendor_xiaomi.git";
   };
