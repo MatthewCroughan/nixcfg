b3ef0c610e55a33c3dea0611abe87e21701a313f
matthewcroughan <matt@croughan.sh>
2022-03-03 22:09:20 +0000

    pyxis: add allRefs to sources
    The branches were screwed with, so the commits still exist but are no
    longer associated with the relevant branches.

--- hosts/pyxis/default.nix
+++ hosts/pyxis/default.nix
@@ -15,23 +15,27 @@
     rev = "2fd487af25b1d688c7528f26cf6f9e92afdddbef";
     ref = "lineage-18.1";
     url = "https://github.com/SDM710-Development/android_device_xiaomi_sdm710-common.git";
+    allRefs = true;
   };
 
   source.dirs."kernel/xiaomi/sdm710".src = builtins.fetchGit {
     rev = "c4d48d66ae60ac6b027bdbb2b618cef6ac245179";
     ref = "10.0-caf";
     url = "https://github.com/SDM710-Development/android_kernel_xiaomi_sdm710.git";
+    allRefs = true;
   };
 
   source.dirs."vendor/xiaomi".src = builtins.fetchGit {
     rev = "cea60d367ffc7375f410f6aac22c531d1b0c3e37";
     ref = "lineage-18.1";
     url = "https://github.com/SDM710-Development/proprietary_vendor_xiaomi.git";
+    allRefs = true;
   };
 
   source.dirs."hardware/xiaomi".src = builtins.fetchGit {
     rev = "d865b86327017fcc5be6658943f38163eb5e2c1f";
     ref = "lineage-18.1";
     url = "https://github.com/LineageOS/android_hardware_xiaomi.git";
+    allRefs = true;
   };
 }
