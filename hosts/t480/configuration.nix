f6a4d50046a2229141568dbaf29c1e29a68d617c
matthewcroughan <matt@croughan.sh>
2022-04-30 22:23:58 +0100

    t480: optimize tlp config

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -67,6 +67,9 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_MAX_PERF_ON_AC = "100";
+        CPU_MAX_PERF_ON_BAT = "30";
         STOP_CHARGE_THRESH_BAT1=95;
         STOP_CHARGE_THRESH_BAT0=95;
       };
