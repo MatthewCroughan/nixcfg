998db80ecdd6045eee377a711b6fb1aa338d9e72
matthewcroughan <matt@croughan.sh>
2022-04-30 22:22:50 +0100

    t480: change i18n to en_GB.UTF-8

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -98,7 +98,7 @@
   };
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
