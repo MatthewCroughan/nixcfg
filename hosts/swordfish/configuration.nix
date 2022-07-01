9a55f17645e93bea3b7a64697e57bd00d6b7e00a
matthewcroughan <matt@croughan.sh>
2022-07-01 17:48:45 +0100

    swordfish: add pasha@newtoncrosby to nix.sshServe.keys

--- hosts/swordfish/configuration.nix
+++ hosts/swordfish/configuration.nix
@@ -30,7 +30,10 @@
       protocol = "ssh-ng";
       enable = true;
       write = true;
-      keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM38GVfgjAWLVRcIJld+Cv4dEg1ais4JSrKjh8dmb9vg julian@goatlap"];
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM38GVfgjAWLVRcIJld+Cv4dEg1ais4JSrKjh8dmb9vg julian@goatlap"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2hSitdOmLziKfsJBeph5T5iUrSjRSCleJuYY8812Mh pasha@newtoncrosby"
+      ];
     };
   };
 
