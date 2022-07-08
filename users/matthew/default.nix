7787534e9d40573a3ccc8b1e54509f9d98c2aa69
matthewcroughan <matt@croughan.sh>
2022-07-08 17:07:32 +0100

    bashrc: improve flash-to function

--- users/matthew/default.nix
+++ users/matthew/default.nix
@@ -36,10 +36,10 @@
         flash-to(){
           if [ $(${pkgs.file}/bin/file $1 --mime-type -b) == "application/zstd" ]; then
             echo "Flashing zst using zstdcat | dd"
-            ${pkgs.zstd}/bin/zstdcat $1 | sudo dd of=$2 status=progress conv=sync,noerror bs=64k
+            ( set -x; ${pkgs.zstd}/bin/zstdcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
           elif [ $(${pkgs.file}/bin/file $2 --mime-type -b) == "application/xz" ]; then
             echo "Flashing xz using xzcat | dd"
-            ${pkgs.xz}/bin/xzcat $1 | sudo dd of=$2 status=progress conv=sync,noerror bs=64k
+            ( set -x; ${pkgs.xz}/bin/xzcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
           else
             echo "Flashing arbitrary file $1 to $2"
             sudo dd if=$1 of=$2 status=progress conv=sync,noerror bs=64k
