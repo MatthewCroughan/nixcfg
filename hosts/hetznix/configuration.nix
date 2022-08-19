8f9d76abd5d9a2417cb355dffc122c414ecef2b7
matthewcroughan <matt@croughan.sh>
2022-08-19 01:58:09 +0100

    hetznix: disable androidUpdate for now

--- hosts/hetznix/configuration.nix
+++ hosts/hetznix/configuration.nix
@@ -3,7 +3,7 @@
   imports = [
     ./disks.nix
     ./hardware-configuration.nix
-    ./modules/androidUpdate.nix
+#    ./modules/androidUpdate.nix
     ./modules/droppers.nix
     ./modules/xandikos.nix
     ./modules/vaultwarden.nix
