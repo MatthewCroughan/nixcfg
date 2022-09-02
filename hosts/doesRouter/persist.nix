d85ec4b5101cb0d00927053031755dc9aa2a1b87
matthewcroughan <matt@croughan.sh>
2022-09-02 20:33:02 +0100

    doesRouter: use files instead of folders to define ssh persistence

--- hosts/doesRouter/persist.nix
+++ hosts/doesRouter/persist.nix
@@ -6,24 +6,12 @@
   };
   environment.persistence."/persist" = {
     directories = [
-      "/etc/ssh"
       "/var/lib/acme"
     ];
     files = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key"
       "/etc/machine-id"
-    ];
-  };
-  services.openssh = {
-    hostKeys = [
-      {
-        path = "/persist/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/persist/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
     ];
   };
 }
