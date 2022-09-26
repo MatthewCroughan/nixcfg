6733d2f1ae807ec2068ea513a7b22fbf84d79db8
matthewcroughan <matt@croughan.sh>
2022-09-26 13:28:16 +0100

    t480: set PCIE_ASPM_ON_BAT to powersupersave

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -63,6 +63,7 @@
     tlp = {
       enable = true;
       settings = {
+        PCIE_ASPM_ON_BAT = "powersupersave";
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_MAX_PERF_ON_AC = "100";
