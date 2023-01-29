d7f78a19df0350b665c4d7a2c0ae5a667e1d5ae1
matthewcroughan <matt@croughan.sh>
2023-01-29 17:22:29 +0000

    h-m: disable parsec-bin, it always has issues fetching the binary via FOD

--- users/matthew/desktop.nix
+++ users/matthew/desktop.nix
@@ -10,7 +10,7 @@
   home = {
     packages = with pkgs; [
       # bypass unfree, without unfreePredicate, etc
-      (parsec-bin.overrideAttrs (_:{ meta.license = null; }))
+#      (parsec-bin.overrideAttrs (_:{ meta.license = null; }))
       quasselClient
       alsa-utils
       wdisplays
