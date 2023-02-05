0ae013e0e6cfe2ceb855e5df7de900d4cc9083ed
matthewcroughan <matt@croughan.sh>
2023-02-05 14:33:27 +0000

    hetznix: serve static i686 version of Nix

--- hosts/hetznix/modules/droppers.nix
+++ hosts/hetznix/modules/droppers.nix
@@ -2,10 +2,11 @@
 let
   mkMemfdDropper = (builtins.getFlake "github:matthewcroughan/nix-dropper-bundle/0cbad762e32fd35f79ed4bb8cff3c63900db6882").bundlers.${pkgs.hostPlatform.system}.memfd_create;
   droppers = map (x: mkMemfdDropper x) [
-    pkgs.hello
-    pkgs.nixStatic
+    pkgs.pkgsStatic.hello
   ];
-  webRoot = pkgs.linkFarmFromDrvs "droppers-webRoot" droppers;
+  webRoot = pkgs.linkFarmFromDrvs "droppers-webRoot" (droppers ++ [
+    (builtins.getFlake "github:nixos/nixpkgs/0874168639713f547c05947c76124f78441ea46c").legacyPackages.i686-linux.pkgsStatic.nix
+  ]);
 in
 {
   services = {
