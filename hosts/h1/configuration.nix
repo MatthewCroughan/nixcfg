387d07294c8d354c1f4f1d087428a5e283f0b9d3
matthewcroughan <matt@croughan.sh>
2023-04-30 13:44:11 +0100

    h1: allow t480 into nix-ssh

--- hosts/h1/configuration.nix
+++ hosts/h1/configuration.nix
@@ -39,6 +39,7 @@
       enable = true;
       write = true;
       keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9gQxVP6k8TNYgkBR+oasyEIooP3QTPmWSkyvywic6 root@t480"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM38GVfgjAWLVRcIJld+Cv4dEg1ais4JSrKjh8dmb9vg julian@goatlap"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2hSitdOmLziKfsJBeph5T5iUrSjRSCleJuYY8812Mh pasha@newtoncrosby"
       ];
