8b0cd8e62c127f0efd250d7e243593b0e00dfb40
matthewcroughan <matt@croughan.sh>
2023-07-27 14:40:38 +0100

    secrets: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,6 +5,7 @@ let
     t480 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ1zPgcnzvK8sUhwX752acg+YkiYR9tyQcZAYj2NAxu root@t480";
     matrix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEw6sYFtowoQMYMrlLSQKOPQ+ZrJmmDNkm1K5X6AGCuz root@matrix";
     hetznix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjpw75X8SXBw8r7b4m4e9eNZoON39E5l+7FBLVZdYVz root@hetznix";
+    doesRouter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBkSKtCYPkOhvDSTJ6Pe8TAN4hcn2+Lkj2zFwf7L1umG";
   };
   users = {
     matthew-t480 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClm+SMN9Bg1HZ+MjH1VQYEXAnslGWT9564pj/KGO79WMQLUxdp3WWa1hQadf2PleAIEFEul3knrpRSEK3yHcCk3g+sCh3XIJcFZLesswe0V+kCAw+JBSd18ESJ4Qko+iDK95cDzucLFwXB10FMVKQCrX90KR+Fp6s6eJHcZGmpxTPgNulDpAjM2APluM3xBCe6zZzt+iNIzn3J8PRKbpNNbuw/LMRU8+udrGbLavUMcSk7ER9pAyLGhz//9aHWDPu7ZRje+vTWgnGFpzbtEzdjnP+2v45nLKWG7o7WdTAsAR8WSccjtNoBiVgSmpHr07zJ0/gTeL4PUkk3lbtzF/PdtTQGm3Ng4SjOBlhRVaTuKBlF2X/Rwq+W4LCbHVgA79MyhJxL2TDbKBPUSLfckqxP89e8Q7iQ4XjIHqVb50ojNNLGcOQRrHq14Twwx/ZDDQvMXCsLwM6vyoYa8KdSaASEr1clx78qNp9PHGlr+UztW+EsoZI7j1tzcHMmq2BSK90= matthew@t480";
@@ -16,7 +17,7 @@ in
 {
   "masariBotsSecrets.age".publicKeys = allUsers ++ [ systems.hetznix ];
   "distributedBuilderKey.age".publicKeys = allUsers ++ [ systems.swordfish systems.t480 ];
-  "cloudflare_api_key.age".publicKeys = allUsers ++ [ systems.swordfish systems.hetznix ];
+  "cloudflare_api_key.age".publicKeys = allUsers ++ [ systems.swordfish systems.hetznix systems.doesRouter ];
   "tunnelvrHerculesClusterJoinToken.age".publicKeys = allUsers ++ [ systems.swordfish ];
   "tunnelvrHerculesBinaryCaches.age".publicKeys = allUsers ++ [ systems.swordfish ];
   "coturn_static_auth.age".publicKeys = allUsers ++ [ systems.matrix ];
